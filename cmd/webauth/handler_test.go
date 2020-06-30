07bc7178a814305383116445de597a4d303fbaa5
Jon Betti <jonbetti@gmail.com>
2020-06-30 00:50:13 +0000

    Update test to account for JS redirect

--- cmd/webauth/handler_test.go
+++ cmd/webauth/handler_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -52,16 +55,25 @@ func TestHandler_ServeHTTP_hasAuthenticatorCredentials(t *testing.T) {
 		Authorizer: testutil.FakeAuthorizer{Email: "jane@example.test"},
 	}
 	r := httptest.NewRequest("get", "https://auth.example.test/", nil)
-	var w httptest.ResponseRecorder
+	w := httptest.ResponseRecorder{Body: &bytes.Buffer{}}
 
 	h.ServeHTTP(&w, r)
 
 	res := w.Result()
-	if res.StatusCode != http.StatusFound {
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
 		t.Errorf("unexpected response status %q", res.Status)
 	}
-	if l := res.Header.Get("Location"); l != "https://site.example.test/some-page" {
-		t.Errorf("unexpected redirect location %q", l)
+	body := mustReadAll(res.Body)
+	expectedBody := `<!doctype html>
+<html lang="en"><meta charset="utf-8">
+<title>Logged in</title>
+<script>window.location.href = "https:\/\/site.example.test\/some-page";</script>
+<h1>Logged in</h1>
+<p>Continue to <a href="https://site.example.test/some-page">https://site.example.test/some-page</a>.
+`
+	if body != expectedBody {
+		t.Errorf("body did not match; got %q, want %q", body, expectedBody)
 	}
 	if cookies := res.Cookies(); len(cookies) != 1 {
 		t.Errorf("expected one cookie; got %v", cookies)
@@ -74,6 +86,14 @@ func TestHandler_ServeHTTP_hasAuthenticatorCredentials(t *testing.T) {
 			t.Errorf("unexpected token %q", c.Value)
 		}
 	}
+}
+
+func mustReadAll(r io.Reader) string {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
 }
 
 func TestHandler_ServeHTTP_hasAuthCookie(t *testing.T) {
